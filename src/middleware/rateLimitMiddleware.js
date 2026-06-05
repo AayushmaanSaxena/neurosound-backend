f690f8175d81d9da04bfc6c198353d5a41a6561a
Aayushmaan Saxena <aymsaxena26@gmail.com>
2026-06-05 11:23:26 +0530

    Home page with real songs and artists from API

--- src/middleware/rateLimitMiddleware.js
+++ src/middleware/rateLimitMiddleware.js
@@ -6,7 +6,7 @@ const rateLimit  = require('express-rate-limit');
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 10, // limit each IP to 10 requests per windowMs
+    max: 1000, // limit each IP to 10 requests per windowMs
     message: {
         message: 'Too many requests from this IP, please try again after 15 minutes'
     },
